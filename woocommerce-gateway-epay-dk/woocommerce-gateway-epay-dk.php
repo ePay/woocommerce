b90245c6cede3a8b2bac17bb6e47522c276cff69
Robert Michalski <robert@surveylegend.com>
2016-06-02 00:41:57 +0200

    Replaced preg_replace with deprecated "e" parameter to preg_replace_callback in jsonRemoveUnicodeSequences function.

--- woocommerce-gateway-epay-dk/woocommerce-gateway-epay-dk.php
+++ woocommerce-gateway-epay-dk/woocommerce-gateway-epay-dk.php
@@ -431,7 +431,13 @@ function add_wc_epay_dk_gateway()
 
 		private function jsonRemoveUnicodeSequences($struct)
 		{
-			return preg_replace("/\\\\u([a-f0-9]{4})/e", "iconv('UCS-4LE','UTF-8',pack('V', hexdec('U$1')))", json_encode($struct));
+			return preg_replace_callback(
+				"/\\\\u([a-f0-9]{4})/",
+				function ($m) {
+					return iconv('UCS-4LE', 'UTF-8', pack('V', hexdec('U' . $m[1])));
+				},
+				json_encode($struct)
+			);
 		}
 
 		/**
